9a142731b27a7aebc6138f2b74754e827d4d86b4
Singrid Maria <singrid.maria@gmail.com>
2024-03-23 11:00:08 -0300

    Ajuste back end

--- main.js
+++ main.js
@@ -1,32 +1,39 @@
-import express from 'express'
-import { connection } from './src/mysql-connect.js'
-import cors from 'cors'
-import sequelize from './src/database/sequelize.js'
+import express from 'express';
+import { connection } from './src/mysql-connect.js';
+import cors from 'cors';
+import morgan from 'morgan';
 
 const port = 8085
 const app = express()
 const routes = express.Router();
 
 app.use(express.json())//Configurar para receber dados do tipo JSON
+app.use(cors());
+app.use(morgan("dev"));
 app.use(routes)//Middleware para controlar requisições --> Intermediador
 
-//ROTAS
-app.get('/', async /*<- Função */ (request, response)  => {
-    const data = connection.execute('select * from user')
-    response.json('App is up')
-    
-    return response.status(200).json(rows);
-    
+// ROTAS
+// app.get('/', async /*<- Função */(request, response) => {
+// const data = connection.execute('select * from user')
+// return response.status(200).json(rows);
+// });
+
+routes.post('\programming-languages', async (request, response) => {
+    try {
+
+
+        const data = request.body
+        const [result] = await connection.execute("INSERT INTO `programming-languages` (`language`,`percentage`) VALUES (?,?)",
+            [data.lang, data.percentage]);
+
+        return response.status(200).json(`Dados criados com sucesso: ${result}`);
+    } catch (error) {
+        return response.status(500).json(error);
+    }
+
 });
 
-app.post('programming-languages',async(request, response) => {
-    const data = request.body
-    const query = await connection.execute('',[])
-
-    return response.json()
-})
-
-app.listen(port,() => {
+routes.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 
 })
